a93754503a0fc414febdb6ca0b7ae9096f7c643f
Matt Hansen <mahans@microsoft.com>
2022-01-07 09:46:46 -0500

    validation logic

--- all-in-one/install.sh
+++ all-in-one/install.sh
@@ -24,7 +24,10 @@ done
 
 # Checking Setup Status
 echo "Waiting for setup to complete"
-until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8080/openmrs/login.htm); do
-    printf '.'
-    sleep 5
+while false; do
+ if [[$(curl http://127.0.0.1:8080/openmrs/login.htm | grep -c username) -eq 0]]; then
+sleep 10
+ fi
 done
+
+echo "Setup is complete"
