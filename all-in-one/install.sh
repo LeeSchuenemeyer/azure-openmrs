b6b51288587d5f8c34440b971238da2a8d8c28c0
Matt Hansen <mahans@microsoft.com>
2022-01-07 10:29:08 -0500

    port assignment

--- all-in-one/install.sh
+++ all-in-one/install.sh
@@ -17,9 +17,9 @@ sudo docker-compose up -d
 
 # Checking Web Service Status
 echo "Waiting for web services"
-until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8080/openmrs/login.htm); do
+until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:80/openmrs/login.htm); do
     printf '.'
-    sleep 5
+    #sleep 5
 done
 
 # Sleeping for DB Setup
