0028af9165425b943b80817e00394cc40db0b4ea
Matt Hansen <mahans@microsoft.com>
2022-01-07 09:04:24 -0500

    script consolidation

--- all-in-one/install.sh
+++ all-in-one/install.sh
@@ -14,3 +14,17 @@ sudo systemctl enable docker
 # Download and run sample docker compose file
 wget https://raw.githubusercontent.com/matthansen0/azure-openmrs/main/all-in-one/docker-compose.yml
 sudo docker-compose up -d
+
+# Checking Web Service Status
+echo "Waiting for web services"
+until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8080/openmrs); do
+    printf '.'
+    sleep 5
+done
+
+# Checking Setup Status
+echo "Waiting for setup to complete"
+until $(curl --output /dev/null --silent --head --fail http://127.0.0.1:8080/openmrs/login.htm); do
+    printf '.'
+    sleep 5
+done
